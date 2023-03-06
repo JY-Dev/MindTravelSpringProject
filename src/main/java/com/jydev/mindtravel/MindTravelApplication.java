cd651ba5db1242251e818b26e16b47b61ba03d4a
jaeyoung <kjy0302014@gmail.com>
2023-03-06 12:18:15 +0900

    Initial commit

---
+++ src/main/java/com/jydev/mindtravel/MindTravelApplication.java
@@ -0,0 +1,13 @@
+package com.jydev.mindtravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindTravelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MindTravelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jydev/mindtravel/MindTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jydev.mindtravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindTravelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
