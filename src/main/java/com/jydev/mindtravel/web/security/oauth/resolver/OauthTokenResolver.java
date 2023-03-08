979c36aa9683005733c5b980f4644ff80bce51f6
jaeyoung <kjy0302014@gmail.com>
2023-03-08 19:57:17 +0900

    [refactor] OauthTokenResolver abstract class -> interface로 수정

--- src/main/java/com/jydev/mindtravel/web/security/oauth/resolver/OauthTokenResolver.java
+++ src/main/java/com/jydev/mindtravel/web/security/oauth/resolver/OauthTokenResolver.java
@@ -3,7 +3,7 @@ package com.jydev.mindtravel.web.security.oauth.resolver;
 import com.jydev.mindtravel.web.security.oauth.model.OauthInfo;
 import com.jydev.mindtravel.web.security.oauth.model.OauthServerType;
 
-public abstract class OauthTokenResolver {
-    abstract public OauthInfo resolve(String token);
-    abstract public boolean supports(OauthServerType type);
+public interface OauthTokenResolver {
+    OauthInfo resolve(String token);
+    boolean supports(OauthServerType type);
 }
