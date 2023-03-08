d0b03c721ff59f84d633f8da9d9cacb86aec581c
jaeyoung <kjy0302014@gmail.com>
2023-03-08 19:15:49 +0900

    [feat] RestTemplate Bean 등록

--- src/main/java/com/jydev/mindtravel/MindTravelApplication.java
+++ src/main/java/com/jydev/mindtravel/MindTravelApplication.java
@@ -2,12 +2,19 @@ package com.jydev.mindtravel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class MindTravelApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MindTravelApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
     }
 
 }
