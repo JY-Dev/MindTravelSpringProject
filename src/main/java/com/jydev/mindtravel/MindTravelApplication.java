3eb59d8427c15832021bf7c72c43ce511708a743
jaeyoung <kjy0302014@gmail.com>
2023-03-09 16:05:35 +0900

    [feat] Jpa Auditing 옵션 활성화

--- src/main/java/com/jydev/mindtravel/MindTravelApplication.java
+++ src/main/java/com/jydev/mindtravel/MindTravelApplication.java
@@ -3,8 +3,10 @@ package com.jydev.mindtravel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.client.RestTemplate;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class MindTravelApplication {
 
