89fe22203216739649f973e267b11e2bdf657edd
jaeyoung <kjy0302014@gmail.com>
2023-03-09 16:57:42 +0900

    [feat] MemberService 생성 및 socialLogin 로직 추가

---
+++ src/main/java/com/jydev/mindtravel/member/service/MemberService.java
@@ -0,0 +1,23 @@
+package com.jydev.mindtravel.member.service;
+
+import com.jydev.mindtravel.member.domain.Member;
+import com.jydev.mindtravel.member.model.MemberDto;
+import com.jydev.mindtravel.member.repository.MemberRepository;
+import com.jydev.mindtravel.web.security.oauth.model.OauthInfo;
+import com.jydev.mindtravel.web.security.oauth.model.OauthServerType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class MemberService {
+    private final MemberRepository repository;
+
+    public MemberDto socialLogin(OauthServerType type, OauthInfo info){
+        Member member = repository.findByEmail(info.getEmail())
+                .orElseGet(() -> repository.save(new Member(type, info)));
+        return new MemberDto(member);
+    }
+}
