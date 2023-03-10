41096bd5c841134389af3154363e2c7017310bd7
jaeyoung <kjy0302014@gmail.com>
2023-03-10 21:34:28 +0900

    [refactor] AuthenticationJwtReturnHandler HttpUtils로 response 보내는 로직 수정

--- src/main/java/com/jydev/mindtravel/web/security/jwt/AuthenticationJwtReturnHandler.java
+++ src/main/java/com/jydev/mindtravel/web/security/jwt/AuthenticationJwtReturnHandler.java
@@ -3,22 +3,22 @@ package com.jydev.mindtravel.web.security.jwt;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jydev.mindtravel.jwt.Jwt;
 import com.jydev.mindtravel.jwt.JwtProvider;
-import com.jydev.mindtravel.web.http.HttpResponse;
-import io.jsonwebtoken.Claims;
+import com.jydev.mindtravel.web.http.HttpUtils;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Component
@@ -26,17 +26,16 @@ import java.util.Map;
 public class AuthenticationJwtReturnHandler implements AuthenticationSuccessHandler {
     private final ObjectMapper mapper;
     private final JwtProvider jwtProvider;
+    private final HttpUtils httpUtils;
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        log.info("로그인 성공 : {}", authentication);
         UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) authentication;
+        JwtClaimsInfo jwtClaimsInfo = new JwtClaimsInfo((String) token.getPrincipal(), token.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()));
+        String test = mapper.writeValueAsString(jwtClaimsInfo);
         Map<String,Object> claims = new HashMap<>();
-        claims.put(JwtProvider.authKey,mapper.writeValueAsString(token));
+        claims.put(JwtProvider.authKey,test);
         Jwt jwt = jwtProvider.createJwt(claims);
-        HttpResponse<Jwt> httpResponse = new HttpResponse<>(HttpServletResponse.SC_OK,"토큰 발급",jwt);
-        String json = mapper.writeValueAsString(httpResponse);
-        response.setStatus(HttpServletResponse.SC_OK);
-        response.setCharacterEncoding("utf-8");
-        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-        response.getWriter().write(json);
+        httpUtils.sendResponse(response,HttpServletResponse.SC_OK,"토큰 발급",jwt);
     }
 }
