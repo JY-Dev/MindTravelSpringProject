77173a77c6a8e0a4b726f86ea331a4bf31c90d06
jaeyoung <kjy0302014@gmail.com>
2023-03-10 23:04:19 +0900

    [feat] MemberController 생성

---
+++ src/main/java/com/jydev/mindtravel/web/controller/MemberController.java
@@ -0,0 +1,24 @@
+package com.jydev.mindtravel.web.controller;
+
+import com.jydev.mindtravel.member.model.MemberDto;
+import com.jydev.mindtravel.web.http.HttpResponse;
+import com.jydev.mindtravel.web.http.HttpUtils;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+public class MemberController {
+    private final HttpUtils httpUtils;
+
+    @GetMapping("/member")
+    public ResponseEntity<HttpResponse<MemberDto>> getMember(@RequestAttribute("member") MemberDto member) {
+        return httpUtils.makeHttpResponse(HttpStatus.OK.value(), "응답 완료",member);
+    }
+}
