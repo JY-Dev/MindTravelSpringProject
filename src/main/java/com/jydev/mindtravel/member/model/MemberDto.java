f29391201575fc5b7c39e76f7b58b26733121027
jaeyoung <kjy0302014@gmail.com>
2023-03-10 10:48:13 +0900

    [feat] MemberDto EqualsAndHashCode 어노테이션 추가

--- src/main/java/com/jydev/mindtravel/member/model/MemberDto.java
+++ src/main/java/com/jydev/mindtravel/member/model/MemberDto.java
@@ -1,11 +1,13 @@
 package com.jydev.mindtravel.member.model;
 
 import com.jydev.mindtravel.member.domain.Member;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@EqualsAndHashCode
 public class MemberDto {
     private String email;
 
