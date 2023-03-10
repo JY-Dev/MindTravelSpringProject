328328d224ca4d8916c52ed73f606254b66a21a3
jaeyoung <kjy0302014@gmail.com>
2023-03-10 22:46:06 +0900

    [refactor] AuthenticationEntryPoint sendResponse 로직 HttpUtils로 처리

--- src/main/java/com/jydev/mindtravel/web/security/AuthenticationEntryPoint.java
+++ src/main/java/com/jydev/mindtravel/web/security/AuthenticationEntryPoint.java
@@ -1,12 +1,11 @@
 package com.jydev.mindtravel.web.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.jydev.mindtravel.web.http.HttpResponse;
+import com.jydev.mindtravel.web.http.HttpUtils;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Component;
 
@@ -16,16 +15,12 @@ import java.io.IOException;
 @RequiredArgsConstructor
 @Component
 public class AuthenticationEntryPoint implements org.springframework.security.web.AuthenticationEntryPoint {
-    private final ObjectMapper mapper;
+    private final HttpUtils httpUtils;
+
     @Override
     public void commence(HttpServletRequest request,
                          HttpServletResponse response,
                          AuthenticationException authException) throws IOException {
-        HttpResponse<String> httpResponse = new HttpResponse<>(HttpServletResponse.SC_UNAUTHORIZED,"인증 실패",null);
-        String json = mapper.writeValueAsString(httpResponse);
-        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        response.setCharacterEncoding("utf-8");
-        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-        response.getWriter().write(json);
+        httpUtils.sendResponse(response, HttpServletResponse.SC_UNAUTHORIZED, "인증 실패", null);
     }
 }
