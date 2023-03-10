3829900088060846faab59f0cc14bd960b29bb34
jaeyoung <kjy0302014@gmail.com>
2023-03-10 22:44:32 +0900

    [feat] HttpUtils makeHttpResponse 메서드 추가

--- src/main/java/com/jydev/mindtravel/web/http/HttpUtils.java
+++ src/main/java/com/jydev/mindtravel/web/http/HttpUtils.java
@@ -3,7 +3,9 @@ package com.jydev.mindtravel.web.http;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -15,9 +17,15 @@ public class HttpUtils {
     public <T> void sendResponse(HttpServletResponse response, int code , String message, T data) throws IOException {
         HttpResponse<T> httpResponse = new HttpResponse<>(code,message,data);
         String json = objectMapper.writeValueAsString(httpResponse);
-        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setStatus(code);
         response.setCharacterEncoding("utf-8");
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.getWriter().write(json);
+    }
+
+    public <R> ResponseEntity<HttpResponse<R>> makeHttpResponse(int code , String message, R data){
+        HttpResponse<R> httpResponse = new HttpResponse<>(code,message,data);
+        return ResponseEntity.status(code)
+                .body(httpResponse);
     }
 }
