b9253568bf05600e2bf0b845e45d3197d00e6205
jaeyoung <kjy0302014@gmail.com>
2023-03-12 12:03:42 +0900

    [refactor] AuthenticationJwtReturnHandler JwtClaimsInfo Authentication으로 생성하는 로직 JwtClaimsInfo로 이동

--- src/main/java/com/jydev/mindtravel/web/security/jwt/AuthenticationJwtReturnHandler.java
+++ src/main/java/com/jydev/mindtravel/web/security/jwt/AuthenticationJwtReturnHandler.java
@@ -11,14 +11,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Component
@@ -27,15 +25,16 @@ public class AuthenticationJwtReturnHandler implements AuthenticationSuccessHand
     private final ObjectMapper mapper;
     private final JwtProvider jwtProvider;
     private final HttpUtils httpUtils;
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         log.info("로그인 성공 : {}", authentication);
         UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) authentication;
-        JwtClaimsInfo jwtClaimsInfo = new JwtClaimsInfo((String) token.getPrincipal(), token.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()));
+        JwtClaimsInfo jwtClaimsInfo = new JwtClaimsInfo(token);
         String test = mapper.writeValueAsString(jwtClaimsInfo);
-        Map<String,Object> claims = new HashMap<>();
-        claims.put(JwtProvider.authKey,test);
+        Map<String, Object> claims = new HashMap<>();
+        claims.put(JwtProvider.authKey, test);
         Jwt jwt = jwtProvider.createJwt(claims);
-        httpUtils.sendResponse(response,HttpServletResponse.SC_OK,"토큰 발급",jwt);
+        httpUtils.sendResponse(response, HttpServletResponse.SC_OK, "토큰 발급", jwt);
     }
 }
--- src/main/java/com/jydev/mindtravel/web/security/jwt/JwtClaimsInfo.java
+++ src/main/java/com/jydev/mindtravel/web/security/jwt/JwtClaimsInfo.java
@@ -1,13 +1,22 @@
 package com.jydev.mindtravel.web.security.jwt;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+
 import java.util.List;
 
-@Builder
 @Getter
 @AllArgsConstructor
-@ToString
 public class JwtClaimsInfo {
     private String email;
     private List<String> authorities;
+
+    public JwtClaimsInfo(UsernamePasswordAuthenticationToken token) {
+        List<String> grantAuthority = token.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority).toList();
+        this.email = (String) token.getPrincipal();
+        this.authorities = grantAuthority;
+    }
 }
