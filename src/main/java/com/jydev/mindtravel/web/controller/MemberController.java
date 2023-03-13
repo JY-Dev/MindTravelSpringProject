6d3ec6552e41982dd270db2fb2b38e1503d335e7
jaeyoung <kjy0302014@gmail.com>
2023-03-13 22:37:57 +0900

    [refactor] MemberController RequestMapping 추가

--- src/main/java/com/jydev/mindtravel/web/controller/MemberController.java
+++ src/main/java/com/jydev/mindtravel/web/controller/MemberController.java
@@ -9,15 +9,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/member")
 public class MemberController {
     private final HttpUtils httpUtils;
 
-    @GetMapping("/member")
+    @GetMapping
     public ResponseEntity<HttpResponse<MemberDto>> getMember(@RequestAttribute("member") MemberDto member) {
         return httpUtils.makeHttpResponse(HttpStatus.OK.value(), "응답 완료",member);
     }
