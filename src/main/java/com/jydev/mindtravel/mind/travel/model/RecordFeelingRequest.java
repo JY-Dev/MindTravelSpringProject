2ca5db45ffe2bf3b79215dc43e228af76ef9493f
jaeyoung <kjy0302014@gmail.com>
2023-03-16 19:19:17 +0900

    [feat] MindTravel RecordFeelingRequest 생성

---
+++ src/main/java/com/jydev/mindtravel/mind/travel/model/RecordFeelingRequest.java
@@ -0,0 +1,9 @@
+package com.jydev.mindtravel.mind.travel.model;
+
+import lombok.Getter;
+
+@Getter
+public class RecordFeelingRequest {
+    private String content;
+    private Feeling feeling;
+}
