d199367168f37c8a123022fa3b425e85c931ab70
jaeyoung <kjy0302014@gmail.com>
2023-03-17 23:21:01 +0900

    [refactor] MoodRecordQueryRepositoryImpl 불필요한 요소 제거

--- src/main/java/com/jydev/mindtravel/mind/travel/repository/MoodRecordQueryRepositoryImpl.java
+++ src/main/java/com/jydev/mindtravel/mind/travel/repository/MoodRecordQueryRepositoryImpl.java
@@ -1,10 +1,8 @@
 package com.jydev.mindtravel.mind.travel.repository;
 
 import com.jydev.mindtravel.mind.travel.domain.MoodRecord;
-import com.querydsl.core.types.dsl.DateTemplate;
 import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.core.types.dsl.StringExpression;
-import com.querydsl.core.types.dsl.StringTemplate;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -17,6 +15,7 @@ import static com.jydev.mindtravel.mind.travel.domain.QMoodRecord.moodRecord;
 @Repository
 public class MoodRecordQueryRepositoryImpl implements MoodRecordQueryRepository {
     private final JPAQueryFactory queryFactory;
+
     @Override
     public List<MoodRecord> searchMoodRecord(String email, String date) {
         StringExpression formattedDate = Expressions.stringTemplate("FUNCTION('DATE_FORMAT', {0}, '%Y-%m-%d')", moodRecord.createdDate);
