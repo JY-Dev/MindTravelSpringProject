aec93b467b65e43215e0c0aa50037fc9ea1a036e
jaeyoung <kjy0302014@gmail.com>
2023-03-17 12:52:19 +0900

    [refactor] MoodRecordRequest AllArgumentConstructor Builder 패턴 적용

--- src/main/java/com/jydev/mindtravel/mind/travel/model/MoodRecordRequest.java
+++ src/main/java/com/jydev/mindtravel/mind/travel/model/MoodRecordRequest.java
@@ -1,8 +1,15 @@
 package com.jydev.mindtravel.mind.travel.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
+@Builder
 public class MoodRecordRequest {
     private String content;
     private Mood mood;
