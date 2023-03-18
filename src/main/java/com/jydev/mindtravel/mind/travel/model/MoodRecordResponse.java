f48b6fbee9121ff5ac7f22020f3a8f1726139ef7
jaeyoung <kjy0302014@gmail.com>
2023-03-18 15:26:34 +0900

    [refactor] MoodRecordResponse Getter가 없어 Jackson에서 처리가 안되서 나는 이슈 수정

--- src/main/java/com/jydev/mindtravel/mind/travel/model/MoodRecordResponse.java
+++ src/main/java/com/jydev/mindtravel/mind/travel/model/MoodRecordResponse.java
@@ -1,10 +1,11 @@
 package com.jydev.mindtravel.mind.travel.model;
 
 import com.jydev.mindtravel.mind.travel.domain.MoodRecord;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
 
-
+@Getter
 public class MoodRecordResponse {
     private String content;
     private Mood mood;
