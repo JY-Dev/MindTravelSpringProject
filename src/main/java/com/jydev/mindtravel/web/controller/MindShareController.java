49417dfbc0ef7b64e85d3e10821b07328270d4ef
jaeyoung <kjy0302014@gmail.com>
2023-03-22 19:20:09 +0900

    [refactor] MindShareController saveMindSharePost에 MindSharePost 저장 로직 추가

--- src/main/java/com/jydev/mindtravel/web/controller/MindShareController.java
+++ src/main/java/com/jydev/mindtravel/web/controller/MindShareController.java
@@ -1,5 +1,8 @@
 package com.jydev.mindtravel.web.controller;
 
+import com.jydev.mindtravel.service.member.model.MemberDto;
+import com.jydev.mindtravel.service.mind.share.model.MindSharePostRequest;
+import com.jydev.mindtravel.service.mind.share.service.MindShareService;
 import com.jydev.mindtravel.web.http.EmptyResponse;
 import com.jydev.mindtravel.web.http.HttpResponse;
 import com.jydev.mindtravel.web.http.HttpUtils;
@@ -7,6 +10,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequiredArgsConstructor
@@ -14,9 +19,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MindShareController {
     private final HttpUtils httpUtils;
-    @PostMapping("/post")
-    public ResponseEntity<HttpResponse<EmptyResponse>> writeMindSharePost(){
+    private final MindShareService mindShareService;
 
+    @PostMapping("/post")
+    public ResponseEntity<HttpResponse<EmptyResponse>> saveMindSharePost(@RequestAttribute("member") MemberDto member,
+                                                                          @RequestBody MindSharePostRequest request) {
+        mindShareService.saveMindSharePost(member.getEmail(),request);
         return httpUtils.makeEmptyResponse();
     }
 }
