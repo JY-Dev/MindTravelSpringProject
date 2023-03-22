85779e619cda014af54d3fbeaf00a0e4ff124870
jaeyoung <kjy0302014@gmail.com>
2023-03-22 19:17:09 +0900

    [feat] MindShareService 생성 및 saveMindSharePost 메서드 추가

--- src/main/java/com/jydev/mindtravel/service/mind/share/domain/MindSharePost.java
+++ src/main/java/com/jydev/mindtravel/service/mind/share/domain/MindSharePost.java
@@ -3,6 +3,7 @@ package com.jydev.mindtravel.service.mind.share.domain;
 import com.jydev.mindtravel.service.base.BaseEntity;
 import com.jydev.mindtravel.service.member.domain.Member;
 import com.jydev.mindtravel.service.mind.share.model.MindSharePostCategory;
+import com.jydev.mindtravel.service.mind.share.model.MindSharePostRequest;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -27,4 +28,13 @@ public class MindSharePost extends BaseEntity {
     @ManyToOne
     @JoinColumn(name = "member_id")
     private Member member;
+
+    public MindSharePost(Member member,MindSharePostRequest request){
+        this.category = request.getCategory();
+        this.title = request.getTitle();
+        this.content = request.getContent();
+        this.likeCount = 0L;
+        this.viewCount = 0L;
+        this.member = member;
+    }
 }
---
+++ src/main/java/com/jydev/mindtravel/service/mind/share/service/MindShareService.java
@@ -0,0 +1,26 @@
+package com.jydev.mindtravel.service.mind.share.service;
+
+import com.jydev.mindtravel.service.exception.ClientException;
+import com.jydev.mindtravel.service.member.domain.Member;
+import com.jydev.mindtravel.service.member.repository.MemberQueryRepository;
+import com.jydev.mindtravel.service.mind.share.domain.MindSharePost;
+import com.jydev.mindtravel.service.mind.share.model.MindSharePostRequest;
+import com.jydev.mindtravel.service.mind.share.repository.MindSharePostCommandRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class MindShareService {
+    private final MemberQueryRepository memberQueryRepository;
+    private final MindSharePostCommandRepository mindSharePostCommandRepository;
+
+    private void saveMindSharePost(String email, MindSharePostRequest request){
+        Member member = memberQueryRepository.findByEmail(email)
+                .orElseThrow(() -> new ClientException("유저 정보가 없습니다."));
+        MindSharePost mindSharePost = new MindSharePost(member,request);
+        mindSharePostCommandRepository.save(mindSharePost);
+    }
+}
