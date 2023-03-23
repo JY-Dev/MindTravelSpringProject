4c4bdf1840df67839dae0927e96e1d6c26e85a34
jaeyoung <kjy0302014@gmail.com>
2023-03-23 16:42:56 +0900

    [refactor] MindSharePostResponse 생성

---
+++ src/main/java/com/jydev/mindtravel/service/mind/share/model/MindSharePostResponse.java
@@ -0,0 +1,23 @@
+package com.jydev.mindtravel.service.mind.share.model;
+
+import com.jydev.mindtravel.service.mind.share.domain.MindSharePost;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class MindSharePostResponse {
+    private String nickname;
+    private String title;
+    private Long likeCount;
+    private Long viewCount;
+    private LocalDateTime createdDate;
+
+    public MindSharePostResponse(MindSharePost mindSharePost) {
+        this.nickname = mindSharePost.getMember().getNickname();
+        this.title = mindSharePost.getTitle();
+        this.likeCount = mindSharePost.getLikeCount();
+        this.viewCount = mindSharePost.getViewCount();
+        this.createdDate = mindSharePost.getCreatedDate();
+    }
+}
