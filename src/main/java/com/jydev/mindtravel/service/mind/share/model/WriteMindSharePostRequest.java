802d8c36b84e83d2cbc477bd43e8a94a3dee1634
jaeyoung <kjy0302014@gmail.com>
2023-03-23 15:37:52 +0900

    [refactor] MindSharePostRequest -> WriteMindSharePostRequest 네이밍 수정

--- src/main/java/com/jydev/mindtravel/service/mind/share/model/WriteMindSharePostRequest.java
+++ src/main/java/com/jydev/mindtravel/service/mind/share/model/WriteMindSharePostRequest.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
-public class MindSharePostRequest {
+public class WriteMindSharePostRequest {
     private String title;
     private String content;
     private MindSharePostCategory category;
