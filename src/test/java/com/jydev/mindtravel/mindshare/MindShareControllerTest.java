32888813aa8026c932f9a171a52aced7c03caa47
jaeyoung <kjy0302014@gmail.com>
2023-03-29 09:56:45 +0900

    [feat] MindShareController searchMindSharePost RestDocs 작성

--- src/test/java/com/jydev/mindtravel/mindshare/MindShareControllerTest.java
+++ src/test/java/com/jydev/mindtravel/mindshare/MindShareControllerTest.java
@@ -2,10 +2,7 @@ package com.jydev.mindtravel.mindshare;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jydev.mindtravel.ControllerTest;
-import com.jydev.mindtravel.service.mind.share.model.MindSharePostCategory;
-import com.jydev.mindtravel.service.mind.share.model.MindSharePostRequest;
-import com.jydev.mindtravel.service.mind.share.model.MindSharePostsRequest;
-import com.jydev.mindtravel.service.mind.share.model.MindSharePostsResponse;
+import com.jydev.mindtravel.service.mind.share.model.*;
 import com.jydev.mindtravel.service.mind.share.service.MindShareService;
 import com.jydev.mindtravel.util.ControllerTestHelper;
 import com.jydev.mindtravel.web.controller.MindShareController;
@@ -24,6 +21,7 @@ import org.springframework.test.web.servlet.ResultActions;
 
 import static com.jydev.mindtravel.ApiDocumentUtils.getDocumentRequest;
 import static com.jydev.mindtravel.ApiDocumentUtils.getDocumentResponse;
+import static com.jydev.mindtravel.mindshare.MindShareMockFactory.getMindSharePostDetailResponse;
 import static com.jydev.mindtravel.mindshare.MindShareMockFactory.getMindSharePostsResponse;
 import static com.jydev.mindtravel.util.ControllerTestHelper.baseRequestBuilder;
 import static org.mockito.ArgumentMatchers.any;
@@ -34,8 +32,7 @@ import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.docu
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
-import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
-import static org.springframework.restdocs.request.RequestDocumentation.queryParameters;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ControllerTest
@@ -117,6 +114,50 @@ public class MindShareControllerTest {
                                 fieldWithPath("data.posts[].viewCount").type(JsonFieldType.NUMBER).description("방문 수"),
                                 fieldWithPath("data.posts[].commentCount").type(JsonFieldType.NUMBER).description("댓글 수"),
                                 fieldWithPath("data.posts[].createdDate").type(JsonFieldType.STRING).description("생성 날짜")
+                        )));
+    }
+
+    @Test
+    public void searchMindSharePostTest() throws Exception {
+        MindSharePostDetailResponse postDetail = getMindSharePostDetailResponse();
+        HttpResponse<MindSharePostDetailResponse> response = new HttpResponse<>(HttpServletResponse.SC_OK, "", postDetail);
+        given(service.searchMindSharePost(any(Long.class))).willReturn(postDetail);
+        given(httpUtils.makeHttpResponse(any(Integer.class), any(String.class), any(MindSharePostDetailResponse.class))).willReturn(
+                ResponseEntity.ok(response)
+        );
+        ResultActions resultActions = this.mockMvc.perform(
+                baseRequestBuilder(
+                        get("/v1/mind/share/post/{postId}",1)
+                )
+        );
+        resultActions.andExpect(status().isOk())
+                .andDo(document("search-mind-share-post-detail",
+                        getDocumentRequest(),
+                        getDocumentResponse(),
+                        requestHeaders(
+                                headerWithName("Authorization").description("Access Token")),
+                        pathParameters(
+                                parameterWithName("postId").description("글 Id")),
+                        responseFields(
+                                fieldWithPath("code").type(JsonFieldType.NUMBER).description("결과코드"),
+                                fieldWithPath("message").type(JsonFieldType.STRING).description("결과메시지"),
+                                fieldWithPath("data.postId").type(JsonFieldType.NUMBER).description("글 Id"),
+                                fieldWithPath("data.nickname").type(JsonFieldType.STRING).description("글 작성자 닉네임"),
+                                fieldWithPath("data.title").type(JsonFieldType.STRING).description("글 제목"),
+                                fieldWithPath("data.content").type(JsonFieldType.STRING).description("글 내용"),
+                                fieldWithPath("data.likeCount").type(JsonFieldType.NUMBER).description("좋아요 수"),
+                                fieldWithPath("data.viewCount").type(JsonFieldType.NUMBER).description("조회수"),
+                                fieldWithPath("data.commentCount").type(JsonFieldType.NUMBER).description("댓글 수"),
+                                fieldWithPath("data.createdDate").type(JsonFieldType.STRING).description("글 생성일"),
+                                fieldWithPath("data.comments[].commentId").type(JsonFieldType.NUMBER).description("댓글 Id"),
+                                fieldWithPath("data.comments[].content").type(JsonFieldType.STRING).description("댓글 내용"),
+                                fieldWithPath("data.comments[].nickname").type(JsonFieldType.STRING).description("댓글 작성자 닉네임"),
+                                fieldWithPath("data.comments[].createdDate").type(JsonFieldType.STRING).description("댓글 생성일"),
+                                fieldWithPath("data.comments[].childComments[].commentId").type(JsonFieldType.NUMBER).description("대댓글 Id"),
+                                fieldWithPath("data.comments[].childComments[].content").type(JsonFieldType.STRING).description("대댓글 내용"),
+                                fieldWithPath("data.comments[].childComments[].nickname").type(JsonFieldType.STRING).description("대댓글 작성자 닉네임"),
+                                fieldWithPath("data.comments[].childComments[].tagNickname").type(JsonFieldType.STRING).description("대댓글 태그 대상 닉네임"),
+                                fieldWithPath("data.comments[].childComments[].createdDate").type(JsonFieldType.STRING).description("대댓글 생성일")
                         )));
     }
 }
--- src/test/java/com/jydev/mindtravel/mindshare/MindShareMockFactory.java
+++ src/test/java/com/jydev/mindtravel/mindshare/MindShareMockFactory.java
@@ -1,6 +1,7 @@
 package com.jydev.mindtravel.mindshare;
 
 import com.jydev.mindtravel.service.member.domain.Member;
+import com.jydev.mindtravel.service.mind.share.domain.MindSharePost;
 import com.jydev.mindtravel.service.mind.share.domain.MindSharePostChildComment;
 import com.jydev.mindtravel.service.mind.share.domain.MindSharePostComment;
 import com.jydev.mindtravel.service.mind.share.model.*;
@@ -9,8 +10,43 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.jydev.mindtravel.member.MemberMockFactory.getMember;
+
 public class MindShareMockFactory {
-    public static MindSharePostResponse getMindSharePostResponse(){
+    public static MindSharePostDetailResponse getMindSharePostDetailResponse() {
+
+        MindSharePostChildCommentResponse childCommentResponse = MindSharePostChildCommentResponse.builder()
+                .commentId(0L)
+                .nickname("nickname")
+                .content("content")
+                .tagNickname("tagNickname")
+                .createdDate(LocalDateTime.now())
+                .build();
+        List<MindSharePostChildCommentResponse> childComments = new ArrayList<>();
+        childComments.add(childCommentResponse);
+        MindSharePostCommentResponse commentResponse = MindSharePostCommentResponse.builder()
+                .childComments(childComments)
+                .content("content")
+                .commentId(0L)
+                .nickname("nickname")
+                .childComments(childComments)
+                .createdDate(LocalDateTime.now()).build();
+        List<MindSharePostCommentResponse> comments = new ArrayList<>();
+        comments.add(commentResponse);
+        return MindSharePostDetailResponse.builder()
+                .comments(comments)
+                .postId(0L)
+                .nickname("nickname")
+                .likeCount(20L)
+                .viewCount(20L)
+                .commentCount(2L)
+                .title("title")
+                .content("content")
+                .createdDate(LocalDateTime.now())
+                .build();
+    }
+
+    public static MindSharePostResponse getMindSharePostResponse() {
         return MindSharePostResponse.builder()
                 .postId(0L)
                 .viewCount(20L)
@@ -22,7 +58,7 @@ public class MindShareMockFactory {
                 .build();
     }
 
-    public static List<MindSharePostResponse> getMindSharePostResponses(long size){
+    public static List<MindSharePostResponse> getMindSharePostResponses(long size) {
         List<MindSharePostResponse> result = new ArrayList<>();
         for (int i = 0; i < size; i++) {
             result.add(getMindSharePostResponse());
@@ -30,7 +66,7 @@ public class MindShareMockFactory {
         return result;
     }
 
-    public static MindSharePostRequest getMindSharePostRequest(MindSharePostCategory category){
+    public static MindSharePostRequest getMindSharePostRequest(MindSharePostCategory category) {
         return MindSharePostRequest.builder()
                 .title("title")
                 .content("content")
@@ -38,7 +74,7 @@ public class MindShareMockFactory {
                 .build();
     }
 
-    public static MindSharePostsRequest getMindSharePostsRequest(MindSharePostCategory category){
+    public static MindSharePostsRequest getMindSharePostsRequest(MindSharePostCategory category) {
         return MindSharePostsRequest.builder()
                 .category(category)
                 .pageSize(10)
@@ -46,46 +82,46 @@ public class MindShareMockFactory {
                 .build();
     }
 
-    public static MindSharePostsResponse getMindSharePostsResponse(long size){
+    public static MindSharePostsResponse getMindSharePostsResponse(long size) {
         return MindSharePostsResponse.builder()
                 .posts(getMindSharePostResponses(size)).totalPostSize(size).build();
     }
 
-    public static MindSharePostCommentRequest getMindSharePostCommentRequest(long postId){
+    public static MindSharePostCommentRequest getMindSharePostCommentRequest(long postId) {
         return MindSharePostCommentRequest.builder()
                 .postId(postId)
                 .content("content")
                 .build();
     }
 
-    public static List<MindSharePostComment> getMindSharePostComments(Member member, long postId, int size){
+    public static List<MindSharePostComment> getMindSharePostComments(Member member, long postId, int size) {
         List<MindSharePostComment> result = new ArrayList<>();
         for (int i = 0; i < size; i++) {
-            result.add(new MindSharePostComment(member,getMindSharePostCommentRequest(postId)));
+            result.add(new MindSharePostComment(member, getMindSharePostCommentRequest(postId)));
         }
         return result;
     }
 
-    public static MindSharePostChildCommentRequest getMindSharePostChildCommentRequest(long parentCommentId){
+    public static MindSharePostChildCommentRequest getMindSharePostChildCommentRequest(long parentCommentId) {
         return MindSharePostChildCommentRequest.builder()
                 .parentCommentId(parentCommentId)
                 .content("content")
                 .build();
     }
 
-    public static List<MindSharePostChildComment> getMindSharePostChildComments(Member member, long parentCommentId, int size){
+    public static List<MindSharePostChildComment> getMindSharePostChildComments(Member member, long parentCommentId, int size) {
         List<MindSharePostChildComment> result = new ArrayList<>();
         for (int i = 0; i < size; i++) {
-            result.add(new MindSharePostChildComment(member,null,getMindSharePostChildCommentRequest(parentCommentId)));
+            result.add(new MindSharePostChildComment(member, null, getMindSharePostChildCommentRequest(parentCommentId)));
         }
         return result;
     }
 
-    public static List<MindSharePostComment> getMindSharePostCommentsWithChildComments(Member member, long postId, int size){
+    public static List<MindSharePostComment> getMindSharePostCommentsWithChildComments(Member member, long postId, int size) {
         List<MindSharePostComment> result = new ArrayList<>();
         for (int i = 0; i < size; i++) {
-            MindSharePostComment comment = new MindSharePostComment(member,getMindSharePostCommentRequest(postId));
-            List<MindSharePostChildComment> childComment = getMindSharePostChildComments(member,i,size);
+            MindSharePostComment comment = new MindSharePostComment(member, getMindSharePostCommentRequest(postId));
+            List<MindSharePostChildComment> childComment = getMindSharePostChildComments(member, i, size);
             comment.addChildComment(childComment);
             result.add(comment);
         }
