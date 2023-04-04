5ea7b07017db7761520eac92915364d4aa168524
jaeyoung <kjy0302014@gmail.com>
2023-04-04 21:02:57 +0900

    [feat] MindShareService deletePostLike 로직 수정 및 insertPostComment, deletePostComment, insertPostChildComment, deletePostChildComment 메서드 추가 및 테스트 코드 작성

--- src/main/java/com/jydev/mindtravel/service/mind/share/service/MindShareService.java
+++ src/main/java/com/jydev/mindtravel/service/mind/share/service/MindShareService.java
@@ -2,15 +2,18 @@ package com.jydev.mindtravel.service.mind.share.service;
 
 import com.jydev.mindtravel.service.exception.ClientException;
 import com.jydev.mindtravel.service.member.domain.Member;
+import com.jydev.mindtravel.service.member.repository.MemberCommandRepository;
 import com.jydev.mindtravel.service.member.repository.MemberQueryRepository;
 import com.jydev.mindtravel.service.mind.share.domain.MindSharePost;
+import com.jydev.mindtravel.service.mind.share.domain.MindSharePostChildComment;
+import com.jydev.mindtravel.service.mind.share.domain.MindSharePostComment;
 import com.jydev.mindtravel.service.mind.share.domain.MindSharePostLike;
+import com.jydev.mindtravel.service.mind.share.model.comment.MindSharePostChildCommentRequest;
+import com.jydev.mindtravel.service.mind.share.model.comment.MindSharePostCommentRequest;
 import com.jydev.mindtravel.service.mind.share.model.comment.MindSharePostCommentResponse;
 import com.jydev.mindtravel.service.mind.share.model.like.MindSharePostLikeResponse;
 import com.jydev.mindtravel.service.mind.share.model.post.*;
-import com.jydev.mindtravel.service.mind.share.repository.MindSharePostCommandRepository;
-import com.jydev.mindtravel.service.mind.share.repository.MindSharePostLikeCommandRepository;
-import com.jydev.mindtravel.service.mind.share.repository.MindSharePostQueryRepository;
+import com.jydev.mindtravel.service.mind.share.repository.*;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -21,53 +24,99 @@ import java.util.List;
 @Transactional
 @Service
 public class MindShareService {
+    private final MemberCommandRepository memberCommandRepository;
     private final MemberQueryRepository memberQueryRepository;
-    private final MindSharePostCommandRepository mindSharePostCommandRepository;
-    private final MindSharePostQueryRepository mindSharePostQueryRepository;
-    private final MindSharePostLikeCommandRepository postLikeCommandRepository;
+    private final MindSharePostCommandRepository postCommandRepository;
+    private final MindSharePostQueryRepository postQueryRepository;
+
+    private final MindSharePostCommentCommandRepository commentCommandRepository;
+    private final MindSharePostChildCommentCommandRepository childCommentCommandRepository;
+    private final MindSharePostLikeCommandRepository likeCommandRepository;
 
     public void saveMindSharePost(String email, MindSharePostRequest request) {
         Member member = memberQueryRepository.findByEmail(email)
                 .orElseThrow(() -> new ClientException("유저 정보가 없습니다."));
         MindSharePost mindSharePost = new MindSharePost(member, request);
-        mindSharePostCommandRepository.save(mindSharePost);
+        postCommandRepository.save(mindSharePost);
     }
 
     public MindSharePostsResponse searchMindSharePosts(MindSharePostsRequest request) {
-        List<MindSharePostResponse> mindSharePosts = mindSharePostQueryRepository.searchMindSharePosts(request);
-        Long totalSize = mindSharePostQueryRepository.searchMindSharePostsTotalSize(request.getCategory());
+        List<MindSharePostResponse> mindSharePosts = postQueryRepository.searchMindSharePosts(request);
+        Long totalSize = postQueryRepository.searchMindSharePostsTotalSize(request.getCategory());
         return new MindSharePostsResponse(totalSize, mindSharePosts);
     }
 
-    public MindSharePostDetailResponse searchMindSharePost(Long postId){
-        MindSharePost post = mindSharePostQueryRepository
+    public MindSharePostDetailResponse searchMindSharePost(Long postId) {
+        MindSharePost post = postQueryRepository
                 .searchMindSharePost(postId).orElseThrow(() -> new ClientException("글이 존재하지 않습니다."));
-        mindSharePostQueryRepository.increaseViewCount(postId);
-        return new MindSharePostDetailResponse(post.getViewCount()+1,post);
+        postQueryRepository.increaseViewCount(postId);
+        return new MindSharePostDetailResponse(post.getViewCount() + 1, post);
     }
 
-    public List<MindSharePostLikeResponse> getPostLikes(Long postId){
-        mindSharePostCommandRepository.findById(postId)
+    public List<MindSharePostLikeResponse> getPostLikes(Long postId) {
+        postCommandRepository.findById(postId)
                 .orElseThrow(() -> new ClientException("글이 존재하지 않습니다."));
-        return mindSharePostQueryRepository.getPostLikes(postId).stream()
+        return postQueryRepository.getPostLikes(postId).stream()
                 .map(MindSharePostLikeResponse::new).toList();
     }
 
-    public List<MindSharePostCommentResponse> getPostComments(Long postId){
-        mindSharePostCommandRepository.findById(postId)
+    public List<MindSharePostCommentResponse> getPostComments(Long postId) {
+        postCommandRepository.findById(postId)
                 .orElseThrow(() -> new ClientException("글이 존재하지 않습니다."));
-        return mindSharePostQueryRepository.getPostComments(postId).stream()
+        return postQueryRepository.getPostComments(postId).stream()
                 .map(MindSharePostCommentResponse::new).toList();
     }
 
-    public void insertPostLike(Long postId, String email){
+    public void insertPostLike(Long postId, String email) {
         Member member = memberQueryRepository.findByEmail(email)
                 .orElseThrow(() -> new ClientException("유저 정보가 없습니다."));
-        postLikeCommandRepository.save(new MindSharePostLike(postId,member));
+        likeCommandRepository.save(new MindSharePostLike(postId, member));
     }
 
-    public void deletePostLike(Long postId, Long memberId){
-        mindSharePostQueryRepository.deleteMindSharePostLike(postId,memberId);
+    public void deletePostLike(Long postId, Long memberId) {
+        postQueryRepository.searchMindSharePostLike(postId, memberId)
+                .ifPresent(likeCommandRepository::delete);
     }
 
+
+    public void insertPostComment(String email, MindSharePostCommentRequest request) {
+        Member member = memberQueryRepository.findByEmail(email)
+                .orElseThrow(() -> new ClientException("유저 정보가 없습니다."));
+        MindSharePostComment comment = new MindSharePostComment(member, request);
+        commentCommandRepository.save(comment);
+    }
+
+    public void deletePostComment(Long commentId, Long memberId) {
+        MindSharePostComment comment = commentCommandRepository.findById(commentId)
+                .orElseThrow(() -> new ClientException("댓글 정보가 없습니다."));
+        boolean isCreator = comment.isCreator(memberId);
+        if (isCreator) {
+            comment.delete();
+        } else {
+            throw new ClientException("댓글을 삭제 할 수 없습니다.");
+        }
+    }
+
+    public void insertPostChildComment(MindSharePostChildCommentRequest request, Long memberId) {
+        Member member = memberCommandRepository.findById(memberId)
+                .orElseThrow(() -> new ClientException("유저 정보가 없습니다."));
+        Member tagMember = null;
+        if (request.hasTagMember()) {
+            tagMember = memberCommandRepository.findById(request.getTagMemberId())
+                    .orElse(null);
+        }
+        MindSharePostChildComment comment = new MindSharePostChildComment(member, tagMember, request);
+        childCommentCommandRepository.save(comment);
+    }
+
+    public void deletePostChildComment(Long commentId, Long memberId) {
+        MindSharePostChildComment comment = childCommentCommandRepository.findById(commentId)
+                .orElseThrow(() -> new ClientException("댓글 정보가 없습니다."));
+        boolean isCreator = comment.isCreator(memberId);
+        if (isCreator) {
+            childCommentCommandRepository.delete(comment);
+        } else {
+            throw new ClientException("댓글을 삭제 할 수 없습니다.");
+        }
+    }
 }
--- src/test/java/com/jydev/mindtravel/mindshare/MindShareServiceTest.java
+++ src/test/java/com/jydev/mindtravel/mindshare/MindShareServiceTest.java
@@ -1,9 +1,15 @@
 package com.jydev.mindtravel.mindshare;
 
+import com.jydev.mindtravel.service.exception.ClientException;
 import com.jydev.mindtravel.service.member.domain.Member;
+import com.jydev.mindtravel.service.member.repository.MemberCommandRepository;
 import com.jydev.mindtravel.service.mind.share.domain.MindSharePost;
+import com.jydev.mindtravel.service.mind.share.domain.MindSharePostChildComment;
+import com.jydev.mindtravel.service.mind.share.domain.MindSharePostComment;
+import com.jydev.mindtravel.service.mind.share.model.comment.MindSharePostChildCommentRequest;
+import com.jydev.mindtravel.service.mind.share.model.comment.MindSharePostCommentRequest;
 import com.jydev.mindtravel.service.mind.share.model.post.MindSharePostDetailResponse;
-import com.jydev.mindtravel.service.mind.share.repository.MindSharePostQueryRepository;
+import com.jydev.mindtravel.service.mind.share.repository.*;
 import com.jydev.mindtravel.service.mind.share.service.MindShareService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -20,9 +26,14 @@ import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class MindShareServiceTest {
-
     @Mock
-    private MindSharePostQueryRepository mindSharePostQueryRepository;
+    private MemberCommandRepository memberCommandRepository;
+    @Mock
+    private MindSharePostCommentCommandRepository commentCommandRepository;
+    @Mock
+    private MindSharePostChildCommentCommandRepository childCommentCommandRepository;
+    @Mock
+    private MindSharePostQueryRepository postQueryRepository;
     @InjectMocks
     private MindShareService mindShareService;
 
@@ -35,9 +46,9 @@ public class MindShareServiceTest {
         given(mindSharePost.getComments()).
                 willReturn(new HashSet<>(MindShareMockFactory.getMindSharePostCommentsWithChildComments(member, postId, size)));
         given(mindSharePost.getMember()).willReturn(member);
-        given(mindSharePostQueryRepository.searchMindSharePost(postId)).willReturn(Optional.of(mindSharePost));
+        given(postQueryRepository.searchMindSharePost(postId)).willReturn(Optional.of(mindSharePost));
         MindSharePostDetailResponse mindSharePostDetailResponse = mindShareService.searchMindSharePost(postId);
-        Assertions.assertThat(mindSharePostDetailResponse.getCommentCount()).isEqualTo(size*size+size);
+        Assertions.assertThat(mindSharePostDetailResponse.getCommentCount()).isEqualTo(size * size + size);
     }
 
     @Test
@@ -49,8 +60,63 @@ public class MindShareServiceTest {
         given(mindSharePost.getComments()).
                 willReturn(new HashSet<>(MindShareMockFactory.getMindSharePostCommentsWithChildComments(member, postId, size)));
         given(mindSharePost.getMember()).willReturn(member);
-        given(mindSharePostQueryRepository.searchMindSharePost(postId)).willReturn(Optional.of(mindSharePost));
+        given(postQueryRepository.searchMindSharePost(postId)).willReturn(Optional.of(mindSharePost));
         MindSharePostDetailResponse mindSharePostDetailResponse = mindShareService.searchMindSharePost(postId);
         Assertions.assertThat(mindSharePostDetailResponse.getComments().get(0).getChildComments().get(0).getTagNickname()).isEmpty();
+    }
+
+    @Test
+    public void deletePostCommentWhenIsNotCommentCreatorTest() {
+        Member commentCreatorMember = mock(Member.class);
+        MindSharePostCommentRequest request = MindSharePostCommentRequest.builder()
+                .postId(0L).content("content").build();
+        MindSharePostComment comment = new MindSharePostComment(commentCreatorMember, request);
+        long requestMemberId = 0L;
+        long commentCreatorId = 1L;
+        given(commentCreatorMember.getId()).willReturn(commentCreatorId);
+        given(commentCommandRepository.findById(any(Long.class))).willReturn(Optional.of(comment));
+        org.junit.jupiter.api.Assertions.assertThrows(ClientException.class, () -> mindShareService.deletePostComment(0L, requestMemberId));
+    }
+
+    @Test
+    public void deletePostChildCommentWhenIsNotCommentCreatorTest() {
+        Member commentCreatorMember = mock(Member.class);
+        MindSharePostChildCommentRequest request = MindSharePostChildCommentRequest.builder()
+                .parentCommentId(0L)
+                .tagMemberId(-1L)
+                .content("content")
+                .build();
+        MindSharePostChildComment comment = new MindSharePostChildComment(commentCreatorMember, null, request);
+        long requestMemberId = 0L;
+        long commentCreatorId = 1L;
+        given(commentCreatorMember.getId()).willReturn(commentCreatorId);
+        given(childCommentCommandRepository.findById(any(Long.class))).willReturn(Optional.of(comment));
+        org.junit.jupiter.api.Assertions.assertThrows(ClientException.class, () -> mindShareService.deletePostChildComment(0L, requestMemberId));
+    }
+
+    @Test
+    public void insertChildCommentWhenHasTagMemberTest() {
+        Member requestMember = mock(Member.class);
+        MindSharePostChildCommentRequest request = MindSharePostChildCommentRequest.builder()
+                .parentCommentId(0L)
+                .tagMemberId(1L)
+                .content("content")
+                .build();
+        given(memberCommandRepository.findById(any(Long.class))).willReturn(Optional.of(requestMember));
+        mindShareService.insertPostChildComment(request, 0L);
+        verify(memberCommandRepository, times(2)).findById(any(Long.class));
+    }
+
+    @Test
+    public void insertChildCommentWhenNotHasTagMemberTest() {
+        Member requestMember = mock(Member.class);
+        MindSharePostChildCommentRequest request = MindSharePostChildCommentRequest.builder()
+                .parentCommentId(0L)
+                .tagMemberId(-1L)
+                .content("content")
+                .build();
+        given(memberCommandRepository.findById(any(Long.class))).willReturn(Optional.of(requestMember));
+        mindShareService.insertPostChildComment(request, 0L);
+        verify(memberCommandRepository, times(1)).findById(any(Long.class));
     }
 }
